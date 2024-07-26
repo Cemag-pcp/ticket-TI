281695b8cac80c60ce337de22397e7dd0dbf56e3
Cemag-pcp <pcp@cemag.com.br>
2024-07-26 16:10:03 -0300

    Primeiro commit

---
+++ accounts/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from .form import RegisterCustomerForm
+
+User = get_user_model()
+
+def register_customer(request):
+    if request.method == 'POST':
+        form = RegisterCustomerForm(request.POST)
+        if form.is_valid():
+            var = form.save(commit=False)
+            var.is_customer = True
+            var.username = var.email
+            var.save()
+            messages.success(request,'Conta criada. Faça o login')
+            return redirect('login')
+        else:
+            messages.warning(request,'Algo deu errado. Preencha os dados corretamente')
+            return redirect('register-customer')
+    else:
+        form = RegisterCustomerForm()
+        context = {'form':form}
+        return render(request,'accounts/register_customer.html',context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None and user.is_active:
+            login(request,user)
+            return redirect('dashboard')
+        else:
+            messages.warning(request,'Algo deu errado. Preecha os dados corretamente')
+            return redirect('login')
+    else:
+        return render(request,'accounts/login.html')
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,'Sessão finalizada. Faça o login novamente para continuar')
+    return redirect('login')
---
+++ dashboard/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def dashboard(request):
+    if request.user.is_customer:
+        return render(request,'dashboard/customer_dashboard.html')
+    elif request.user.is_engineer:
+        return render(request,'dashboard/engineer_dashboard.html')
